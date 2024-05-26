0ce3f66dd647f2d7dece238c980925ab4790b937
kashish2633 <148215379+kashish2633@users.noreply.github.com>
2024-05-26 14:54:18 +0530

    Add files via upload

---
+++ Day 1/src/FirstClassD1.java
@@ -0,0 +1,21 @@
+
+	import java.util.*;
+
+	public class FirstClassD1 {
+
+	  public static void main(String args[]){
+	    System.out.println("Hello ");
+	    int a,b,c;
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter a value : ");
+	    a=sc.nextInt();
+	    System.out.println("Enter b value : ");
+	    b=sc.nextInt();
+	    
+	    System.out.println ("Addition of 2 num : "+(a+b));
+	    System.out.println ("Subtraction of 2 num  "+(a-b));
+	    System.out.println ("Multiplication of 2 num : "+(a*b));
+	    System.out.println ("Division of 2 num : "+(a/b));
+	    System.out.println ("Module of 2 num : "+(a%b)); 
+	  }
+	}
